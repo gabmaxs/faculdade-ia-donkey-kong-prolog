b375b85a748d744d13d6337dd9c2b803d470187d
Gabriel Santos <gabmaxsan@gmail.com>
2019-11-26 16:37:18 -0300

    add martelo and peach

--- jogo.pl
+++ jogo.pl
@@ -6,6 +6,12 @@ pertence(E, [_|C]) :- pertence(E,C).
 concatena([],L,L).
 concatena([Cb|Cd],L,[Cb|Cd2]) :- concatena(Cd,L,Cd2).
 
+retirar_elemento(E,[E|C],C).
+retirar_elemento(E,[E1|C],[E1|C1]) :- retirar_elemento(E,C,C1).
+
+inverter([],[]).
+inverter([E|C],Ln) :- inverter(C,Cn),concatena(Cn,[E],Ln).
+
 % MOVIMENTACAO
 sobe([X,Y],[Xnovo,Y]) :- X < 4, Xnovo is X + 1. % SOBE
 desce([X,Y],[Xnovo,Y]) :- X > 0, Xnovo is X - 1. % BAIXO
@@ -13,9 +19,10 @@ anda([X,Y],[X,Ynovo]) :- Y > 0, Ynovo is Y - 1. % ESQUERDA
 anda([X,Y],[X,Ynovo]) :- Y < 9, Ynovo is Y + 1. % DIREITA 
 
 % CONDICOES DE MOVIMENTACAO
-pode_andar(Estado, Proximo, _, ListaParedes, _, Caminho) :- % ANDAR PARA O LADO / PAREDES
+pode_andar(Estado, Proximo, _, ListaParedes, ListaBarril, Caminho) :- % ANDAR PARA O LADO / PAREDES
     anda(Estado,Proximo),
     not(pertence(Proximo, ListaParedes)),
+    not(pertence(Proximo, ListaBarril)),
     not(pertence(Proximo, [Estado|Caminho])).
 
 
@@ -30,6 +37,12 @@ pode_andar(Estado, Proximo, ListaEscadas, _, _, Caminho) :- % DESCER / ESCADA
     not(pertence(Proximo, [Estado|Caminho])).
 
 % pode_andar(Estado, Proximo, _, _, ListaBarril, Caminho) :-
+%     not(pertence(Proximo, [Estado|Caminho])),
+%     pertence(Proximo, ListaBarril),
+%     anda(Estado, Proximo),
+%     pode_andar(Proximo, _, _, _, ListaBarril, Caminho).
+
+% pode_andar(Estado, Proximo, _, _, ListaBarril, Caminho) :-
 %     anda(Estado,Proximo),
 %     not(pertence(Proximo, [Estado|Caminho])).
 
@@ -39,18 +52,20 @@ estende([Estado|Caminho], ListaCaminhos, ListaEscadas, ListaParedes, ListaBarril
     bagof([Proximo,Estado|Caminho], (pode_andar(Estado, Proximo, ListaEscadas, ListaParedes, ListaBarril, Caminho)), ListaCaminhos),!.
 estende(_,[],_, _, _).
 
+solucao(Inicial, ListaEscadas, ListaParedes, ListaBarril, Martelo, Peach, Solucao) :-
+    busca_largura([[Inicial]], ListaEscadas, ListaParedes, ListaBarril, Martelo, BuscaMaterlo),
+    busca_largura([[Martelo]], ListaEscadas, ListaParedes, ListaBarril, Peach, BuscaPeach),
+    inverter(BuscaMaterlo,NM),
+    inverter(BuscaPeach,NP),
+    concatena(NM,NP, Solucao).
 
-meta(X) :- X = [4,9].
-    
-
-solucao(Inicial, ListaEscadas, ListaParedes, ListaBarril, S) :- busca_largura([[Inicial]], ListaEscadas, ListaParedes, ListaBarril, S).
-busca_largura([[Posicao|Caminho]|_], _, _, _, [Posicao|Caminho]) :- meta(Posicao).
-busca_largura([Posicao|Calda], ListaEscadas, ListaParedes, ListaBarril, Caminho) :- 
+busca_largura([[Posicao|Caminho]|_], _, _, _, Destino, [Posicao|Caminho]) :- Posicao = Destino, Caminho \= [].
+busca_largura([Posicao|Calda], ListaEscadas, ListaParedes, ListaBarril, Destino, Caminho) :- 
     estende(Posicao, ListaProximos, ListaEscadas, ListaParedes, ListaBarril),
     concatena(Calda,ListaProximos, NovaListaProximos),
-    busca_largura(NovaListaProximos, ListaEscadas, ListaParedes, ListaBarril, Caminho).
+    busca_largura(NovaListaProximos, ListaEscadas, ListaParedes, ListaBarril, Destino, Caminho).
 
 
 % FUNCAO PRINCIPAL
-jogo(Inicial, ListaEscadas, ListaParedes, ListaBarril, C) :- 
-    solucao(Inicial, ListaEscadas, ListaParedes, ListaBarril, C).
+jogo(Inicial, ListaEscadas, ListaParedes, ListaBarril, Martelo, Peach, C) :- 
+    solucao(Inicial, ListaEscadas, ListaParedes, ListaBarril, Martelo, Peach, C).
